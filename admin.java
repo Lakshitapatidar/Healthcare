f4a6b6125d2a4b98e2401de46fffb9b1774f42b2
Lakshitapatidar <patidarlakshita01@gmail.com>
2024-01-18 01:02:12 +0530

    admin file also added

---
+++ admin.java
@@ -0,0 +1,9 @@
+public class admin {
+    admin(){
+        System.out.println("press 1: Add doctor");
+        System.out.println("press 2:Remove doctor");
+        System.out.println("press 3:view patientList");
+        System.out.println("press 4:view drList");
+        System.out.println("press 5:see feedback");
+    }
+}
